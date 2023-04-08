c9dbb2d9cf719e634fa4613078a95cd6c1506d08
Novo <120156959+Novo1999@users.noreply.github.com>
2023-04-08 19:34:52 +0600

    khasio calculator done

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+let screenValue = '';
 
-const buttonPress = document.getElementById('button-click');
-buttonPress.addEventListener('click', function(){
-    console.log('clicked');
-}); 
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach((btn)=>{
+    btn.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == '='){
+            screenValue = eval(screenValue);
+            document.querySelector('input').value = screenValue;
+        }
+        else if(e.target.innerHTML == 'C'){
+            screenValue = '';
+            document.querySelector('input').value = screenValue;
+        }
+        else{
+            screenValue = screenValue + e.target.innerHTML;
+            document.querySelector('input').value = screenValue;
+        }
+    });
+});
 
